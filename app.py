0608acb43f6487dc0421729042646196787105c0
Scc_hy <296294812@qq.com>
2024-01-13 10:11:10 +0800

    [ LLM-MedQA ]- 🎉 Init: gr

--- app.py
+++ app.py
@@ -3,17 +3,8 @@
 # Author: Scc_hy
 # Func: web demo
 # ==============================================================================
-"""
-This script refers to the dialogue example of streamlit, the interactive generation code of chatglm2 and transformers.
-We mainly modified part of the code logic to adapt to the generation of our model.
-Please refer to these links below for more information:
-    1. streamlit chat example: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
-    2. chatglm2: https://github.com/THUDM/ChatGLM2-6B
-    3. transformers: https://github.com/huggingface/transformers
-"""
-
+import gradio as gr
 from dataclasses import asdict
-import streamlit as st
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from appPrepare.interface import GenerationConfig, generate_interactive
@@ -24,102 +15,94 @@ warnings.filterwarnings('ignore')
 
 # os.system(f'sh ./appPrepare/env_prepare.sh')
 mg_path = dl_mg()
-
-def on_btn_click():
-    del st.session_state.messages
-
-
-@st.cache_resource
-def load_model():
-    model = (
-        AutoModelForCausalLM.from_pretrained(mg_path, trust_remote_code=True)
-        .to(torch.bfloat16)
-        .cuda()
-    )
-    tokenizer = AutoTokenizer.from_pretrained(mg_path, trust_remote_code=True)
-    return model, tokenizer
-
-
-def prepare_generation_config():
-    with st.sidebar:
-        max_length = st.slider("Max Length", min_value=32, max_value=2048, value=2048)
-        top_p = st.slider("Top P", 0.0, 1.0, 0.8, step=0.01)
-        temperature = st.slider("Temperature", 0.0, 1.0, 0.7, step=0.01)
-        st.button("Clear Chat History", on_click=on_btn_click)
-
-    generation_config = GenerationConfig(max_length=max_length, top_p=top_p, temperature=temperature)
-
-    return generation_config
-
-
+generation_config = GenerationConfig(max_length=2048, top_p=0.01, temperature=0.01)
 user_prompt = "<|User|>:{user}\n"
 robot_prompt = "<|Bot|>:{robot}<eoa>\n"
 cur_query_prompt = "<|User|>:{user}<eoh>\n<|Bot|>:"
 
 
-def combine_history(prompt):
-    messages = st.session_state.messages
+def combine_history(prompt, chat_history):
+    """
+    chat_history [q, a]
+    """
     total_prompt = ""
-    for message in messages:
-        cur_content = message["content"]
-        if message["role"] == "user":
-            cur_prompt = user_prompt.replace("{user}", cur_content)
-        elif message["role"] == "robot":
-            cur_prompt = robot_prompt.replace("{robot}", cur_content)
-        else:
-            raise RuntimeError
-        total_prompt += cur_prompt
+    for message in chat_history:
+        cur_prompt = user_prompt.replace("{user}", message[0])
+        itotal_prompt += cur_prompt
+        cur_prompt = robot_prompt.replace("{robot}", message[1])
+        itotal_prompt += cur_prompt
     total_prompt = total_prompt + cur_query_prompt.replace("{user}", prompt)
     return total_prompt
 
 
-def main():
-    # torch.cuda.empty_cache()
-    print("load model begin.")
-    model, tokenizer = load_model()
-    print("load model end.")
+class Model_center():
+    def __init__(self):
+        # 构造函数，加载检索问答链
+        self.model = (
+            AutoModelForCausalLM.from_pretrained(mg_path, trust_remote_code=True)
+            .to(torch.bfloat16)
+            .cuda()
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(mg_path, trust_remote_code=True)
 
-    user_avator = "pic/user.png"
-    robot_avator = "pic/robot.png"
+    def qa_answer(self, question: str, chat_history: list = []):
+            if question == None or len(question) < 1:
+                return "", chat_history
+            try:
+                real_prompt = combine_history(question, chat_history)
+                out = ''
+                for cur_response in generate_interactive(
+                    model=self.model,
+                    tokenizer=self.tokenizer,
+                    prompt=real_prompt,
+                    additional_eos_token_id=103028,
+                    **asdict(generation_config),
+                ):
+                    out += cur_response
 
-    st.title("InternLM-Chat-7B")
-
-    generation_config = prepare_generation_config()
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"], avatar=message.get("avatar")):
-            st.markdown(message["content"])
-
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        with st.chat_message("user", avatar=user_avator):
-            st.markdown(prompt)
-        real_prompt = combine_history(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt, "avatar": user_avator})
-
-        with st.chat_message("robot", avatar=robot_avator):
-            message_placeholder = st.empty()
-            for cur_response in generate_interactive(
-                model=model,
-                tokenizer=tokenizer,
-                prompt=real_prompt,
-                additional_eos_token_id=103028,
-                **asdict(generation_config),
-            ):
-                # Display robot response in chat message container
-                message_placeholder.markdown(cur_response + "▌")
-            message_placeholder.markdown(cur_response)
-        # Add robot response to chat history
-        st.session_state.messages.append({"role": "robot", "content": cur_response, "avatar": robot_avator})
-        torch.cuda.empty_cache()
+                chat_history.append((question, out))
+                # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+                return "", chat_history
+            except Exception as e:
+                return e, chat_history
 
 
-if __name__ == "__main__":
-    main()
+
+# 实例化核心功能对象
+model_center = Model_center()
+# 创建一个 Web 界面
+block = gr.Blocks()
+with block as demo:
+    with gr.Row(equal_height=True):   
+        with gr.Column(scale=15):
+            # 展示的页面标题
+            gr.Markdown("""<h1><center>InternLMDoctor</center></h1>
+                <center>书生浦语-Doctor</center>
+                """)
+    with gr.Row():
+        with gr.Column(scale=4):
+            # 创建一个聊天机器人对象
+            chatbot = gr.Chatbot(height=450, show_copy_button=True)
+            # 创建一个文本框组件，用于输入 prompt。
+            msg = gr.Textbox(label="Prompt/问题")
+
+            with gr.Row():
+                # 创建提交按钮。
+                db_wo_his_btn = gr.Button("Chat")
+            with gr.Row():
+                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                clear = gr.ClearButton(
+                    components=[chatbot], value="Clear console")
+    
+        db_wo_his_btn.click(model_center.qa_answer, inputs=[
+                            msg, chatbot], outputs=[msg, chatbot])
+
+    gr.Markdown("""提醒：<br>
+    1. 初始化数据库时间可能较长，请耐心等待。
+    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    """)
+
+
+gr.close_all()
+# 直接启动
+demo.launch()
